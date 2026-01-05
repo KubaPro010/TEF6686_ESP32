59f85a375db35992a1d6d1ca0f717c333ab1c97a
KubaPro010 <kuba.pro010@gmail.com>
2026-01-05 11:53:35 +0100

    Transition to PlatformIO

---
+++ include/NTPupdate.h
@@ -0,0 +1,28 @@
+#ifndef NTP_H
+#define NTP_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+#include "ESP32Time.h"
+#include "TimeLib.h"
+#include "TEF6686.h"
+
+static const char ntpServerName[] = "0.pool.ntp.org";
+static const int localPort = 8944;
+const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
+
+extern ESP32Time rtc;
+extern WiFiClient RemoteClient;
+extern WiFiUDP Udp;
+extern TEF6686 radio;
+
+extern bool wifi;
+extern bool NTPupdated;
+extern bool rtcset;
+
+void sendNTPpacket(IPAddress &address);
+void NTPupdate();
+time_t getNtpTime();
+#endif
---
+++ lib/TFT_eSPI/User_Setup.h
@@ -0,0 +1,18 @@
+//#define ARS                 // uncomment for BGR type display (ARS version)
+#define DEEPELEC_DP_66X     // uncomment for DEEPELEC Portable DP-66X build
+
+#define ILI9341_DRIVER
+#define TFT_DMA_SUPPORT
+#define TFT_CS          5
+#define TFT_DC          17
+#define TFT_RST         16
+#define TOUCH_CS	32
+
+#define SPI_FREQUENCY       7500000
+#define SPI_READ_FREQUENCY 20000000
+#define SPI_TOUCH_FREQUENCY 2500000
+#define SMOOTH_FONT
+
+#ifdef ARS
+#define TFT_RGB_ORDER TFT_RGB
+#endif
